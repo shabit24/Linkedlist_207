ff24c9e673b672d0dc300d35842427fb5fd0df92
shabit24 <hossain.shabit24@gmail.com>
2023-05-19 16:10:08 +0700

    main function declare

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,7 +9,7 @@ struct Node
 };
 
 Node* START = NULL;
-void addNote() {
+void addNode() {
 	int nim;
 	string nama;
 	Node* nodeBaru = new Node();
@@ -21,13 +21,20 @@ void addNote() {
 	nodeBaru->name = nama;
 
 	if (START == NULL || nim <= START->noMhs) {
-		cout << "NIM sudah ada" << endl;
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+	   
+		nodeBaru->next = START;
+		START = nodeBaru;
 		return;
-
 	}
 
-	Node* current = START;
 	Node* previous = START;
+	Node* current = START;
+	
 	
 	while ((current != NULL) && (nim >= current->noMhs))
 	{
@@ -47,13 +54,28 @@ bool searchNode(int nim, Node* current, Node* previous) {
 	previous = START;
 	current = START;
 
-}
-while ((current != NULL) && (nim >= current->noMhs))
 
-	if ()
+   while (current != NULL && nim > current->noMhs)
 
+
+{
 	previous = current;
 	current = current->next;
+}
+
+		if (current == NULL) 
+		{
+			return false;
+		}
+		else if (current->noMhs == nim)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+}
 
 	bool deleteNode(int nim) {
 		Node* current = START;
@@ -68,24 +90,46 @@ while ((current != NULL) && (nim >= current->noMhs))
 
 	bool listEmpty()
 	{
-		if (listEmpty())
-			if ((START = NULL))
+			if ((START == NULL))
 				return true;
 			else
 				return false;
 	}
 		
-		{
+	bool listEmpty() {
+		if (START == NULL)
+			return true;
+		else
+			return false;
+
+	}
+
+	void traverse() {
+		if (listEmpty()) {
 			cout << "List Kosong" << endl;
 			system("pause");
 			system("cls");
 			return;
 		}
+
 		else {
+			int nim;
+			cout << "Masukkan NIM: ";
+			cin >> nim;
 			Node* currentNode = START;
-			while (currentNode != NULL)
-			{
-				cout<<"Nim: " currentNode->noMhs<<",Nama"
+			while (currentNode != NULL) {
+				if (currentNode->noMhs == nim) {
+					cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+					return;
+				}
+				currentNode = currentNode->next;
+
 			}
+			cout << "Data tida ditemukan " << endl;
 
 		}
+
+	}
+	
+	int main() {
+
